6df5bdb74b0b3ce56f01136b2f7225022e2c2721
mrajaonson <m.rajaonson@pm.me>
2026-06-26 16:28:07 +0200

    init backend

---
+++ backend/src/test/java/net/rajaonson/room_management_system/RoomManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.rajaonson.room_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
